5ccbb887c07527b179e0cf9e62cfa72d00b4e6a8
Alex Yucra <xalextrack@gmail.com>
2026-03-18 16:54:01 -0300

    init

--- flai/cli/__init__.py
+++ flai/cli/__init__.py
@@ -7,6 +7,6 @@ __version__ = "1.0.0"
 __author__ = "Alex Yucra"
 
 # Exportar funções principais
-from flai.cli.utils2.ollama_manager import run_ollama, get_ollama_manager
+# from flai.cli.utils2.ollama_manager import run_ollama, get_ollama_manager
 
-__all__ = ['run_ollama', 'get_ollama_manager']
+# __all__ = ['run_ollama', 'get_ollama_manager']
